1a6022aafed7623efec0af2eaa359f7097732414
Hamida Rahimi <hamidarahimi154@gmail.com>
2024-10-12 21:09:00 +0430

    Add files via upload

---
+++ pythone assingnment.py
@@ -0,0 +1,28 @@
+# 1 what is OOP?
+#  Object-Oriented Programming (OOP) is a programming paradigm that uses "objects" to design software.
+#  It allows for the encapsulation of data and behavior, making it easier to manage and scale applications.
+class animal
+def(self ):
+return "some sound"
+
+# 2 what is a class? 
+#  A class is a blueprint for creating objects.
+#  It defines a set of attributes (data) and methods (functions) that the created objects will have.
+
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+# 3  What is a Constructor?
+
+#A constructor is a special method called when an object is instantiated.
+It typically initializes the attributes of the object.
+
+
+#4  What is Inheritance?
+#Inheritance is a mechanism where a new class (child class) can inherit attributes and methods from another class (parent class).
+ #This promotes code reusability.
+
+
+ 
